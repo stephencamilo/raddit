ae6faa61074b48f0170187a558501c35892b4e65
Stephen Camilo <stephen.camilo@live.com>
2020-05-23 16:16:30 -0300

    Add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_023232) do
+ActiveRecord::Schema.define(version: 2020_05_23_173014) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "link_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_023232) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
